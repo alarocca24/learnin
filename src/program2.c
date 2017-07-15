841d8913b5d006892246a540662844f9374735bc
Alex L <alex@secondspectrum.com>
2017-07-15 08:17:22 -0700

    more edits to the program2 file. I feel like the functions are correct. However the loops and loop terminations is what is causing me funky problems.

--- src/program2.c
+++ src/program2.c
@@ -5,40 +5,50 @@
 const int WORK_WEEK_MAX_HOURS=40;
 const int MAX_NAME=25;
 
-int overTime(float hoursWorked, float payRate) {
-  float overtimeHrs, overtimePay;
+int overTimeHrs(float hoursWorked, float payRate) {
+  float overtimeHrs;
   overtimeHrs = hoursWorked - WORK_WEEK_MAX_HOURS;
-  overtimePay = overtimeHrs * (payRate * 1.5);
-  return overtimePay;
+  return overtimeHrs;
+}
+
+int overtimePay(float hoursWorked, float payRate) {
+  float otPay, otRate;
+  otRate = 1.5 * payRate;
+  otPay = overTimeHrs(hoursWorked, payRate) * otRate;
+  return otPay;
 }
 
 int grossPay(float hoursWorked, float payRate) {
-  float totalPay;
-  totalPay = hoursWorked * payRate;
+  float gross, otHrs, totalPay;
+  otHrs = overTimeHrs(hoursWorked, payRate);
 
-  if (hoursWorked > WORK_WEEK_MAX_HOURS) {
-    totalPay += overTime(hoursWorked, payRate);
-  }else {
-    return totalPay;
+  if (hoursWorked <= WORK_WEEK_MAX_HOURS) {
+    gross = hoursWorked * payRate;
+  } else {
+    gross = hoursWorked * payRate;
+    gross += (otHrs * (1.5 * payRate));
   }
-  return totalPay;
+  totalPay = hoursWorked * payRate;
+  return gross;
 }
 
-
-int basePay()  {
-  float basePayTotal;
-  basePayTotal = grossPay() - overTime();
+int basePay(float hoursWorked, float payRate) {
+  float basePayTotal=0;
+  if (hoursWorked <= WORK_WEEK_MAX_HOURS) {
+    basePayTotal = grossPay(hoursWorked, payRate);
+  } else {
+    basePayTotal = grossPay(hoursWorked, payRate) - overtimePay(hoursWorked, payRate);
+  }
   return basePayTotal;
 }
 
-
-int taxesPaid() {
+int taxesPaid(float hoursWorked, float payRate) {
   float taxRate = 0.2;
-  return grossPay() * taxRate;
+  return grossPay(hoursWorked, payRate) * taxRate;
 }
 
-int netPay() {
-  return grossPay() - taxesPaid();
+int netPay(float hoursWorked, float payRate) {
+  return grossPay(hoursWorked, payRate) - taxesPaid(hoursWorked, payRate);
 }
 
 int main() {
@@ -51,16 +61,16 @@ int main() {
   for (i=0; i < maxLoop; i++) {
 
     printf("Enter name: ");
-    scanf("%s", &name[i]);
-    if (strcmp(name[i], "-1") == 0) {
+    scanf("%s", &name[0][i]);
+    if (strcmp(name, "-1") == 0) {
       break;
     }
-    printf("\nEnter hourly rate: ");
+    printf("Enter hourly rate: ");
     scanf("%f", &payRate[i]);
     if (payRate[i] == -1) {
       break;
     }
-    printf("\nEnter hours worked: ");
+    printf("Enter hours worked: ");
     scanf("%f", &hoursWorked[i]);
     printf("\n");
     if (hoursWorked[i] == -1) {
@@ -68,16 +78,23 @@ int main() {
     }
   }
 
-  maxLoop = i;
-
   for (i=0; i < maxLoop; i++) {
-      printf("Pay To: %s\n", name[i]);
-      printf("Hours Worked: %0.2f\n", payRate[i]);
-      printf("Hourly Rate: $%0.2f\n", hoursWorked[i]);
-      printf("Gross Pay: $%0.2f\n", grossPay(hoursWorked[i], payRate[i]));
-      printf("Base Pay: $%0.2f\n", basePay());
-      printf("Overtime Pay: $%0.2f", overTime(hoursWorked[i], payRate[i]));
-      printf("Taxes Paid: $%0.2f", taxesPaid());
-      printf("Net Pay: $%0.2f", netPay());
+    float gross, ot, base, tax, net;
+
+    gross = grossPay(hoursWorked[i], payRate[i]);
+    ot = overtimePay(hoursWorked[i], payRate[i]);
+    base = basePay(hoursWorked[i], payRate[i]);
+    tax = taxesPaid(hoursWorked[i], payRate[i]);
+    net = netPay(hoursWorked[i], payRate[i]);
+
+    printf("\nPay To: %s\n", name[MAX_NAME][i]);
+    printf("Hours Worked: %0.2f\n", hoursWorked[i]);
+    printf("Hourly Rate: $%0.2f\n", payRate[i]);
+    printf("Gross Pay: $%0.2f\n", gross);
+    printf("Base Pay: $%0.2f\n", base);
+    printf("Overtime Pay: $%0.2f\n", ot);
+    printf("Taxes Paid: $%0.2f\n\n", tax);
+    printf("--------------\n");
+    printf("Net Pay: $%0.2f\n", net);
   }
 }
